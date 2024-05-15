c00cf2430a6ae5657b47890158b03923ab69dff9
timepresent95 <timepresent95@gmail.com>
2024-05-15 23:29:40 +0900

    summoner search api response 수정

--- 2.page/summoner/lib/loader.ts
+++ 2.page/summoner/lib/loader.ts
@@ -1,10 +1,7 @@
 import { getSummonerInfoByAccount } from "@/4.features/SummonerSearch/api";
-import { Summoner } from "@/5.entities/Summoner/model";
 import { LoaderFunction } from "react-router-dom";
 
-export const summonerLoader: LoaderFunction = async ({
-  params,
-}): Promise<Summoner> => {
+export const summonerLoader: LoaderFunction = async ({ params }) => {
   const { gameName, tagLine } = params;
   if (gameName === undefined || tagLine === undefined) {
     // 에러 핸들링 필요
--- 4.features/SummonerSearch/api.ts
+++ 4.features/SummonerSearch/api.ts
@@ -1,10 +1,16 @@
+import { League, LeagueEntry } from "@/5.entities/League/model";
 import { Summoner } from "@/5.entities/Summoner/model";
 import { fetchQuery } from "@/6.shared/lib/api";
+
+interface Response {
+  summoner: Summoner;
+  leagueEntries: LeagueEntry & { League: League };
+}
 
 export async function getSummonerInfoByAccount(
   gameName: string,
   tagLine: string
-): Promise<Summoner> {
+): Promise<Response> {
   const path = `/summoner/${gameName}/${tagLine}`;
   return await fetchQuery(path, { method: "get" }, [
     `summoner-${gameName}-${tagLine}`,
