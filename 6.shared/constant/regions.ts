a898e203d5d69b7e975449e3d1bc32599ad8950e
timepresent95 <timepresent95@gmail.com>
2024-06-07 01:12:29 +0900

    상수값 수정 및 추가

--- 6.shared/constant/regions.ts
+++ 6.shared/constant/regions.ts
@@ -1,6 +1,2 @@
-export const regionHost = {
-  AMERICAS: { region: "AMERICAS", host: "americas.api.riotgames.com" },
-  ASIA: { region: "ASIA", host: "asia.api.riotgames.com" },
-  EUROPE: { region: "EUROPE", host: "europe.api.riotgames.com" },
-  SEA: { region: "SEA", host: "sea.api.riotgames.com" },
-};
+export const regions = ["AMERICAS", "ASIA", "EUROPE", "SEA"] as const;
+export type Region = (typeof regions)[number];
